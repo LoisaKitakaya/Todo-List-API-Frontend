4cf4fed25668f0e60dea9927940d36c7cc1ade3a
Freedom Loisa <kitakayaloisa@gmail.com>
2022-02-26 08:13:54 +0300

    designed todo list frontend

---
+++ js/main.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+  var load_list = $("#load-list");
+  var load_list_spinner = $("#load-list-spinner");
+  var create_account_btn = $("#create-account-btn");
+  var create_account_btn_spinner = $("#create-account-btn-spinner");
+  var generate_token_btn = $("#generate-token-btn");
+  var generate_token_btn_spinner = $("#generate-token-btn-spinner");
+  var create_todo_btn = $("#create-todo-btn");
+  var create_todo_btn_spinner = $("#create-todo-btn-spinner");
+  var update_todo_btn = $("#update-todo-btn");
+  var update_todo_btn_spinner = $("#update-todo-btn-spinner");
+
+  load_list_spinner.hide();
+  create_account_btn_spinner.hide();
+  generate_token_btn_spinner.hide();
+  create_todo_btn_spinner.hide();
+  update_todo_btn_spinner.hide();
+});
